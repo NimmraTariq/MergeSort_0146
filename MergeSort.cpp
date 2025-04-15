55f63e47fce2fd0094553a11042f71d8920f7309
NimmraTariq <nimra369tariq@gmail.com>
2025-04-15 11:58:33 +0700

    Initializing the values for our varisbles

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -42,10 +42,11 @@ void mergeSort(int low, int high)
 
     int mid = (low + high) / 2;
 
-    
+
     mergeSort(low, mid);
     mergeSort(mid + 1, high);
 
+    
     int i = low;
     int j = mid + 1;
     int k = low;
